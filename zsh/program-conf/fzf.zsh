e20db199104ca153d0a565c124ed61e40e20995c
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-02-21 12:27:29 +0100

    moved fzf.zsh to program-conf instead of plugin and created the folders for it. also zsh-vi-mode.zsh is now zsh-vim-mode.zsh

--- zsh/program-conf/fzf.zsh
+++ zsh/program-conf/fzf.zsh
@@ -4,10 +4,10 @@
 #  / /\__ \ | | |
 # /___|___/_| |_|
 
-# zsh fzf plugin config by Tristan Lukens
+# zsh fzf config by Tristan Lukens
 # tristanlukens.github.io
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
-export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+export FZF_DEFAULT_OPTS="--height 60% --layout=reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
