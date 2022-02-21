e7dad7a681292099de36720684f82e726e706abf
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-02-21 12:23:39 +0100

    added kanye autostart function for zsh

--- zsh/autostart.zsh
+++ zsh/autostart.zsh
@@ -7,5 +7,16 @@
 # zsh autostart config by Tristan Lukens
 # tristanlukens.github.io
 
-fortune -s | cowsay
-echo   # empty line for spacing
+fc() {
+  fortune -s | cowsay
+  echo  # empty line for spacing
+}
+
+kanye() {
+  quote=$(curl -s api.kanye.rest | jq ".quote")
+  echo "$quote \n — Kanye West" | cowsay && echo  # empty line for spacing
+}
+
+# ----------------
+
+kanye
