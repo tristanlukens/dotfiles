fc5089a3fffb18298c22ea4ec92ef829cc121adb
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-02-22 12:38:11 +0100

    someone forgot to stage a directory again... definately wasn't me

---
+++ program-conf/README.md
@@ -0,0 +1,5 @@
+# program-conf
+
+The configurations of command line interfaces that need to be set up in your zshrc/bashrc/....
+
+I'm importing them in zshrc.
---
+++ program-conf/bat.zsh
@@ -0,0 +1,10 @@
+#          _
+#  _______| |__
+# |_  / __| '_ \
+#  / /\__ \ | | |
+# /___|___/_| |_|
+
+# bat config by Tristan Lukens
+# tristanlukens.github.io
+
+export BAT_THEME="gruvbox-dark"
