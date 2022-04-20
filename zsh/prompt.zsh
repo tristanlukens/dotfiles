6f9c70306d9d7095531dd199a5d669db448aa5d4
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-04-20 18:57:38 +0200

    I swear this is the last time

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -14,4 +14,4 @@ theme=2
  
 [[ $theme = 0 ]] && PROMPT="%B%K{cyan} %n %k %K{208} %F{black}in%f %k %~ λ %b"
 [[ $theme = 1 ]] && PROMPT="%F{black}%B%K{45} %n %k %K{208} in%f %k %~ λ %b"
-[[ $theme = 2 ]] && PROMPT="%B%K{cyan} %~ %k%b > "
+[[ $theme = 2 ]] && PROMPT="%B%K{cyan} %~ %k%b -> "
